f2cd00ae1c99d870a9b47d33db0a8fe405c182b3
oazambranoa <oazambranoa@unal.edu.co>
2024-09-25 11:13:10 -0500

    Y zoom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef, useState } from "react";
+import { Stage, Layer, Rect } from "react-konva";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import { Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Header from "./Components/Header";
 
 function App() {
+  const stageRef = useRef(null);
+  const width = 500;
+  const height = 500;
+
+  const [stage, setStage] = useState({
+    scale: 1,
+    x: 0,
+    y: 0,
+  });
+
+  const scaleRelativeToPoint = (point, increaseScale) => {
+    const scaleBy = 1.02;
+    const stage = stageRef.current;
+    const oldScale = stage.scaleY();
+    const mousePointTo = {
+      x: point.x / oldScale - stage.x() / oldScale,
+      y: point.y / oldScale - stage.y() / oldScale,
+    };
+
+    const newScale = increaseScale ? oldScale * scaleBy : oldScale / scaleBy;
+
+    setStage({
+      scale: newScale,
+      x: (point.x / newScale - mousePointTo.x) * newScale,
+      y: (point.y / newScale - mousePointTo.y) * newScale,
+    });
+  };
+
+  const handleWheel = (e) => {
+    e.evt.preventDefault();
+    scaleRelativeToPoint(
+        e.target.getStage().getPointerPosition(),
+        e.evt.deltaY < 0
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Header />
+        <Container fluid>
+          <Row>
+            <Col xs={9}>
+              <Row className="">
+                <Button
+                    variant="primary"
+                    onClick={() => {
+                      scaleRelativeToPoint(
+                          {
+                            x: width / 2,
+                            y: height / 2,
+                          },
+                          true
+                      );
+                    }}
+                >
+                  +
+                </Button>{" "}
+                <Button
+                    variant="secondary"
+                    onClick={() => {
+                      scaleRelativeToPoint(
+                          {
+                            x: width / 2,
+                            y: height / 2,
+                          },
+                          false
+                      );
+                    }}
+                >
+                  -
+                </Button>{" "}
+              </Row>
+              <Row className="d-grid justify-content-center canvasborder">
+                <Stage
+                    className="mt-5 ml-5"
+                    width={width}
+                    height={height}
+                    onWheel={handleWheel}
+                    scaleY={stage.scale}
+                    x={0}
+                    y={10}
+                    ref={stageRef}
+                >
+                  <Layer>
+                    <Rect fill="Blue" height={100} width={100} />
+                    <Rect fill="Red" height={1} width={50} />
+                  </Layer>
+                </Stage>
+              </Row>
+            </Col>
+          </Row>
+          <h1>{stage.scale}</h1>
+        </Container>
+      </>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg border bg-dark">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto"></div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
